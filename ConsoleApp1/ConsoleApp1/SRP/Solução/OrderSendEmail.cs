8318950a50a10dc0717ece29eb4aef26bdeadd7c
Tun1n <slpedro21@outlook.com>
2025-11-06 13:36:13 -0300

    SRP

---
+++ ConsoleApp1/ConsoleApp1/SRP/Solução/OrderSendEmail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.SRP.Solução
+{
+    public class OrderSendEmail
+    {
+        public void SendEmail()
+        {
+            Console.WriteLine("Email enviado ao cliente!");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/SRP/Violação/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.SRP.Violação
+{
+    // A classe Order está violando o Princípio da Responsabilidade Única (SRP) pois ela possui mais de uma responsabilidade.
+    public class Order
+    {
+        public void Print()
+        {
+            Console.WriteLine("Pedido feito!");
+        }
+
+        public void SendEmail()
+        {
+            Console.WriteLine("Email enviado ao cliente!");
+        }
+
+    }
+}
+
